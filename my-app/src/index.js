51ac4f7aaabfb7e80dfea54ea25821a12c9d1985
Sara Oscarsson <oscarsson.sara@hotmail.com>
2021-06-03 15:43:03 +0200

    history

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -52,12 +52,13 @@ const Square = (props) => {
         history: [{
           squares: Array(9).fill(null)
         }],
+        stepNumber: 0,
         xIsNext: true
       };
     }
   
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -68,13 +69,20 @@ const Square = (props) => {
         history: history.concat([{
           squares: squares
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
+      });
+    }
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
       });
     }
     
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
 
       const moves = history.map((step, move) => {
@@ -82,7 +90,7 @@ const Square = (props) => {
           'Go to move #' + move :
           'Go to game start';
         return (
-          <li>
+          <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
         );
